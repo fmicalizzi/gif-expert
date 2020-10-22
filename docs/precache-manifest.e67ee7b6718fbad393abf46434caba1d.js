0a671744ff5372e54c4369aa2f36c29421cabf43
Franco Micalizzi <franco.micalizzi@gmail.com>
2020-10-21 20:12:03 -0500

    add docs

---
+++ docs/precache-manifest.e67ee7b6718fbad393abf46434caba1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc8915d24061695a0e88c0b53c5e7a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9aa9a6eb06a53a4b59f",
+    "url": "/static/css/main.297bbc58.chunk.css"
+  },
+  {
+    "revision": "e915424eeb4a1675bf19",
+    "url": "/static/js/2.7e8e1ec7.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.7e8e1ec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9aa9a6eb06a53a4b59f",
+    "url": "/static/js/main.ee0a9b53.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
